9ab203dd894c155444cf9575db477ab33ae0bf3e
luping <165340815@qq.com>
2014-02-28 16:59:09 +0800

    luping0228

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeixinJob::Application.config.secret_token = '36efe97b8a89951796d4eddd7165df61365b6e0eef897d865d508fb987b59b6803f9f78af89f4e6739d67a34e074195f5f1f536d72a2b6543cc1c504dfdb58f0'
