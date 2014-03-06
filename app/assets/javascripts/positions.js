5d3fac816adfeb3737e5d2289bde99ef8b16a0c8
wangping <525399584@qq.com>
2014-03-06 13:56:52 +0800

    postions

--- app/assets/javascripts/positions.js
+++ app/assets/javascripts/positions.js
@@ -1,3 +1,8 @@
-function position_edit(){
-    alert(1231);
+function position_edit(obj){
+     var id = $(obj).find(".id").val() ;
+     var description = $(obj).find(".description").val() ;
+     var name = $(obj).find(".name").val() ;
+     ($(".jobInfo").find('.id').val(id));
+     ($(".jobInfo").find('input[type=text]').val(name));
+     ($(".jobInfo").find('textarea').val(description));
 }
--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -16,24 +16,43 @@ class PositionsController < ApplicationController   #招聘职位
   end
   
   def new
+    admin = Company.client
     @position = Position.new
     @positions = @company.positions.paginate(page:params[:page],per_page: PerPage)
   end
 
   def create
-     name = params[:positions][:name]
-     description = params[:positions][:description]
-     @position = Position.new
-     @position.name = name
-     @position.description = description
-     @position.company_id = @company.id
-     if @position.save
-       flash[:success] = "新建成功！"
-       redirect_to company_positions_path(@company)
-     else
-       flash[:error] = "新建失败！职位已经存在！"
-       render 'new'
-     end
+    id = params[:positions][:id]
+    if id == ""
+      name = params[:positions][:name]
+      description = params[:positions][:description]
+      @position = Position.new
+      @position.name = name
+      @position.description = description
+      @position.company_id = @company.id
+      if @position.save
+        flash[:success] = "新建成功！"
+        redirect_to company_positions_path(@company)
+      else
+        flash[:error] = "新建失败！职位已经存在！"
+        render 'new'
+      end
+    else
+      update
+    end
+  end
+  def update
+    id = params[:positions][:id]
+    name = params[:positions][:name]
+    description = params[:positions][:description]
+    @position = Position.find_by_id(id)
+    if @position && @position.update_attributes(name:name,description:description)
+      flash[:success] = "更新成功！"
+      redirect_to company_positions_path(@company)
+    else
+      flash[:error] = "更新失败！职位不存在！"
+      render 'new'
+    end
   end
 
   def release   #发布
