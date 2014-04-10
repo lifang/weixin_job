0b62901a8716c5eabfb0b184a559e0c762acb962
wangping <525399584@qq.com>
2014-04-10 15:33:57 +0800

    wp

--- app/assets/javascripts/common/resumes.js
+++ app/assets/javascripts/common/resumes.js
@@ -1,6 +1,6 @@
 $(function(){
     $("#form_div").sortable();
-    //$("#form_div .itemBox").draggable();
+//$("#form_div .itemBox").draggable();
 })
 
 function remove_form_item(obj, name){     //删除简历模板中的某个元素
@@ -187,27 +187,29 @@ function create_resume_valid(obj){
     }
 }
 function change_status(company_id,p_and_r_id,status){
-    $.ajax({
-        url:"/companies/"+company_id+"/resumes/"+p_and_r_id+"/change_status",
-        data:"status="+status,
-        dataType:'text',
-        success :function(d){
-            if(d==1){
-                tishi_alert("操作成功！");
-                location.reload();
-            }else{
-                tishi_alert("操作失败！");
-            }
+    if(confirm("确认拒绝？")){
+        $.ajax({
+            url:"/companies/"+company_id+"/resumes/"+p_and_r_id+"/change_status",
+            data:"status="+status,
+            dataType:'text',
+            success :function(d){
+                if(d==1){
+                    tishi_alert("操作成功！");
+                    location.reload();
+                }else{
+                    tishi_alert("操作失败！");
+                }
 
-        }
-    });
+            }
+        });
+    }
 }
 
 function deal_audition(company_id,p_and_r_id){
-  $("#audition_form").attr("action","/companies/"+company_id+"/resumes/"+p_and_r_id+"/deal_audition");
+    $("#audition_form").attr("action","/companies/"+company_id+"/resumes/"+p_and_r_id+"/deal_audition");
 }
 function deal_join(company_id,p_and_r_id){
-  $("#join_form").attr("action","/companies/"+company_id+"/resumes/"+p_and_r_id+"/deal_join");
+    $("#join_form").attr("action","/companies/"+company_id+"/resumes/"+p_and_r_id+"/deal_join");
 }
 function submit_audition_form(obj){
     var form = $(obj).parents("#audition_form");
@@ -255,20 +257,20 @@ function search_positon_resumes(obj){
         return false;
     }
     var start = $(form).find("input[name='start']").val();
-     if(start==""){
+    if(start==""){
         tishi_alert("开始时间不能为空！");
         return false;
     }
     var end = $(form).find("input[name='end']").val();
-     if(end==""){
+    if(end==""){
         tishi_alert("结束时间不能为空！");
         return false;
     }
     form.submit();
 }
 function show_the_position(obj){
-  var content = $(obj).find("option:selected").text();
-  $(obj).parent().find(".search_position").html(content);
+    var content = $(obj).find("option:selected").text();
+    $(obj).parent().find(".search_position").html(content);
 }
 function show_resume(company_id,client_resume_id){
     location.href = "/companies/"+company_id+"/resumes/show_resume?client_resume_id="+client_resume_id;
